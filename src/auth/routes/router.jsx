369d133fd679dd72d8f31a890ff2fdb49c8c7465
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-04-11 14:35:02 +0600

    Root, Header, Footer and Home component create

--- src/auth/routes/router.jsx
+++ src/auth/routes/router.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router";
+import ErrorPage from "../../components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <h1>Home Router</h1>,
+    errorElement: <ErrorPage />,
   },
 ]);
 
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <div className="container mx-auto">
+        Your demandding page not found!!!!!
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
